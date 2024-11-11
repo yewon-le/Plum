630ba0ddc84dc6ae4947b02401145ee51ffbd44e
NAYUN YOUK <nayunk10@gmail.com>
2024-11-11 12:29:57 +0900

    delete main copy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
-import Main from "./pages/MainPage/Main";
+import Main from "./pages/MainPage/Main copy";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Drawer from "./components/Drawer";
 import { useState } from "react";
@@ -19,8 +19,8 @@ function App() {
         <Drawer open={isLeftDrawerOpen} side="left" />
         <div className="content-wrapper">
           <Routes>
-            <Route path="main" element={<Main />} />
-            <Route path="/" element={<Main />} />
+            <Route path="main" element={<Main/>} />
+            <Route path="/" element={<Main/>} />
           </Routes>
         </div>
         <Drawer open={isRightDrawerOpen} side="right" />
