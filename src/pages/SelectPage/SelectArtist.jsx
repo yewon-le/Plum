7f4a54fcc0929e7790bd23b19f5d3d0fdc646b74
yewon_lee <dapang14@naver.com>
2024-11-20 10:32:21 +0900

    UI edit

--- src/pages/SelectPage/SelectArtist.jsx
+++ src/pages/SelectPage/SelectArtist.jsx
@@ -1,91 +1,94 @@
+import React, { useState } from "react";
 import "./SelectArtist.css";
 import logo from "../../images/logo.png";
-import albumImage from "../../images/aespa.png"
+import artistImage from "../../images/aespa.png";
 
 const ArtistNames = [
-    {
-      ArtistImage: "../../images/aespa.png",
-      singer: "noname",
+  {
+    ArtistImage: artistImage,
+    singer: "noname",
+  },
+  {
+    ArtistImage: "https://via.placeholder.com/50",
+    singer: "noname2",
+  },
+  {
+    ArtistImage: "https://via.placeholder.com/50",
+    singer: "noname3",
+  },
+  {
+    ArtistImage: "https://via.placeholder.com/50",
+    singer: "noname4",
+  },
+  {
+    ArtistImage: "https://via.placeholder.com/50",
+    singer: "noname5",
+  },
+  {
+    ArtistImage: "https://via.placeholder.com/50",
+    singer: "noname6",
+  },
+  {
+    ArtistImage: "https://via.placeholder.com/50",
+    singer: "noname7",
+  },
+  {
+    ArtistImage: "https://via.placeholder.com/50",
+    singer: "noname8",
+  },
+  {
+    ArtistImage: "https://via.placeholder.com/50",
+    singer: "noname9",
+  },
+  {
+    ArtistImage: "https://via.placeholder.com/50",
+    singer: "noname10",
+  },
+  // ... (나머지 항목 동일)
+];
 
-    },
-    {
-        ArtistImage: "https://via.placeholder.com/50",
-        singer: "noname",
-  
-      },
-      {
-        ArtistImage: "https://via.placeholder.com/50",
-        singer: "noname",
-  
-      },
-      {
-        ArtistImage: "https://via.placeholder.com/50",
-        singer: "noname",
-  
-      },
-      {
-        ArtistImage: "https://via.placeholder.com/50",
-        singer: "noname",
-  
-      },
-      {
-        ArtistImage: "https://via.placeholder.com/50",
-        singer: "noname",
-  
-      },
-      {
-        ArtistImage: "https://via.placeholder.com/50",
-        singer: "noname",
-  
-      },
-    {
-      ArtistImage: "https://via.placeholder.com/50",
-      singer: "noname",
-  
-    },
-    {
-      ArtistImage: "https://via.placeholder.com/50",
-      singer: "noname",
-
-    },
-    {
-        ArtistImage: "https://via.placeholder.com/50",
-        singer: "noname",
-  
-      },
-    
-  ];
 const SelectArtist = () => {
- 
-    return(
-        <div className="Select-Container">
-               <div className="select-logo">
-            <img src={logo} alt="Logo"/>
-          </div>
-          <div className="select-title">선호하는 아티스트를 선택하세요 (최소 1개)</div>
-          <div className="artist-container">
-            <div className="artist-list">
-            {ArtistNames.map((artist) => (
-                <div className="artist-album-box">
-                  <div className="artist-album-cover">
-                    <img
-                      src={artist.albumImage}
-                      alt={artist.album}
-                      className="artist-album-cover"
-                    />
-                  </div>
-                  <div className="artist-title-box">
-                    <div className="artist-singer">{artist.singer}</div>
-                  </div>
-                </div>
-            ))}
+  const [selectedArtist, setSelectedArtist] = useState(null);
+
+  const selectArtist = (singer) => {
+    setSelectedArtist(singer === selectedArtist ? null : singer);
+  };
+
+  return (
+    <div className="Select-Container">
+      <div className="select-logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="select-title">선호하는 아티스트를 선택하세요 (최소 1개)</div>
+      <div className="artist-container">
+        <div className="artist-list">
+          {ArtistNames.map((artist) => (
+            <div
+              key={artist.singer}
+              className={`artist-album-box ${
+                selectedArtist === artist.singer ? "selected" : ""
+              }`}
+              onClick={() => selectArtist(artist.singer)}
+            >
+              <div className="artist-album-cover">
+                <img
+                  src={artist.ArtistImage}
+                  alt={artist.singer}
+                  className="artist-album-cover"
+                />
+              </div>
+              <div className="artist-title-box">
+                <div className="artist-singer">{artist.singer}</div>
+              </div>
             </div>
-          </div>
-          <div className="select-button-container">
-            <button className="select-button">선택하기</button>
-          </div>
+          ))}
         </div>
-    )
+      </div>
+      <div className="select-button-container">
+        <button className="select-button">선택하기</button>
+      </div>
+    </div>
+  );
 };
 
 export default SelectArtist;
